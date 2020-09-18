fcafdd8a210261caee45f7bd11e475119f7ccd47
MWLeonel <wmleonel@gmail.com>
2020-09-18 09:30:50 -0300

    Somar 2 Numeros

---
+++ Soma2Numeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("Digite o 1ºValor: ");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2ºValor: ");
+            valor2 = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine($"Seu resultado é: {valor1 + valor2}");
+
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+        }
+    }
+}
